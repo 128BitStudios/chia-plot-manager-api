02142cdb4a722087c355be3293a48c8798ec7575
Brad Bird <brad@128bits.co.uk>
2021-05-18 01:47:53 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -13,7 +14,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
+  res.send('<h1>Anybody there?!</h1>');
+});
+
+app.get('/webhook/ping', (req: Request, res: Response) => {
+  res.send('Pong!');
+});
+
+app.post('/webhook', (req: Request, res: Response) => {
+  try {
+    const timestamp = `[${new Date().toISOString()}]`
+    const log = `${timestamp} ${req.body.content}`;
+    console.log(log);
+    fs.writeFileSync(`logs/main.log`, log + "\n", { flag: 'a', encoding: 'utf-8' });
+  } catch (error: any) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/logs/full', (req: Request, res: Response) => {
+  try {
+    const logs = fs.readFileSync(`logs/main.log`, 'utf-8');
+    res.send(logs.replace(/['"]+/g, ''));
+  } catch (error: any) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
