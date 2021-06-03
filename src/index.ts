8ed6cce6ec65779cecc11cf3fa2dc9169bf6b591
Brad Bird <brad@128bits.co.uk>
2021-06-03 23:54:08 +0100

    prod ready fixes

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,10 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req: Request, res: Response) => {
+  res.send({ data: 'Pong' })
+});
+
 app.get('/ping', (req: Request, res: Response) => {
   res.send({ data: 'Pong' })
 });
