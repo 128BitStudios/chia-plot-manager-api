6d8ea2965bbf0b97200bd35954ae4ddd09ec81fd
Brad Bird <brad@128bits.co.uk>
2021-06-03 22:54:37 +0100

    add kubernetes deploy scripts

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t cpm -f Dockerfile .
---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker tag cpm ghcr.io/128bitstudios/chia-plot-manager-api:latest
+docker push ghcr.io/128bitstudios/chia-plot-manager-api:latest
